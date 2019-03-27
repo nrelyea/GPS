6dfad928d54b2f64b33f4902861b74aae9e50eb4
Nathan Relyea <relyean19@up.edu>
2019-03-26 19:23:46 -0700

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace GPS
+{
+    public partial class Form1 : Form
+    {
+        int gridSize = 0;
+
+        public Form1(int gS)
+        {
+            InitializeComponent();
+            gridSize = gS;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+        }
+
+        public void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+            drawPoint(e, 200, 400);
+
+        }
+
+        private void tnrAppTimer_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        public void drawPoint(System.Windows.Forms.PaintEventArgs e, int x, int y)
+        {
+            TextFormatFlags flags = TextFormatFlags.Bottom | TextFormatFlags.EndEllipsis;
+
+            int pointSize = 10;
+
+            Rectangle pt = new Rectangle(x, y, pointSize, pointSize);
+            System.Drawing.SolidBrush redBrush = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
+            e.Graphics.FillEllipse(redBrush, pt);
+            e.Graphics.DrawEllipse(Pens.Black, pt);
+
+            //TextRenderer.DrawText(e.Graphics, "p", this.Font,
+            //new Rectangle(x + pointSize, y + (pointSize / 2), 40, 20), SystemColors.ControlText, flags);
+
+
+
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("HELLO"); 
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new GPS.Form1(5));
+
+        }
+
+    }    
+}
